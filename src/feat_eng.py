908fb6680ab7b1c1c4788153bdf9f424af3831a4
OpareKwafo <oparekwafo@gmail.com>
2023-07-06 10:45:35 -0600

    completed functions and process function

--- src/feat_eng.py
+++ src/feat_eng.py
@@ -1,47 +1,17 @@
 import pandas as pd
-
-
-def merge_dataframe(dataframe_to_keep: pd.DataFrame, dataframe_to_add: pd.DataFrame, merging_columns: str, merging_method: str) -> pd.DataFrame:
-    """
-    Merges 2 or more DataFrames.
-
-    Args:
-        dataframe_to_keep (pd.DataFrame): DataFrame you want to keep all information.
-        dataframe_to_add (pd.DataFrame): DataFrame we want to add.
-        merging_columns (pd.DataFrame): common columns to merge on.
-        merging_method (pd.DataFrame): decides how to perform the merge.
-
-    Returns:
-        pd.DataFrame: DataFrame with the new DataFrame added.
-    """
-    results = dataframe_to_keep.merge(dataframe_to_add, on=merging_columns, how=merging_method)
-    return results
     
-    
-def fill_null_with_zeros(dataframe: pd.DataFrame) -> pd.DataFrame:
-    """
-    Fills the null values in a DataFrame with zeros.
-
-    Args:
-        dataframe (pd.DataFrame): Pandas DataFrame containing missing or null values.
-
-    Returns:
-        pd.DataFrame: New Pandas DataFrame with null values filled with zeros.
-    """
-    return dataframe.fillna(0)
-
-def create_dataframe(dataframe: pd.DataFrame, columns_to_extract: str) -> pd.DataFrame:
+def create_dataframe(sales_df: pd.DataFrame, columns_to_extract: list[str]) -> pd.DataFrame:
     """
     This function creates a new DataFrame from an existing DataFrame.
 
     Args:
-        dataframe (pd.DataFrame): The existing Pandas DataFrame containing our interested features.
+        dataframe (pd.DataFrame): The existing sales DataFrame containing our interested features.
         columns_to_extract (str): The columns we need in our new DataFrame
 
     Returns:
         pd.DataFrame: New DataFrame with our interested features.
     """
-    new_dataframe = dataframe[columns_to_extract]
+    new_dataframe = sales_df[columns_to_extract]
     new_dataframe = new_dataframe.drop_duplicates()
     return new_dataframe
 
@@ -57,16 +27,13 @@ def extract_datetime_features(dataframe: pd.DataFrame, column_name: str) -> pd.D
     Returns:
         pd.DataFrame: DataFrame with new datetime features.
     """
-  
-    if dataframe[column_name].dtype == 'datetime64[ns]':
-        dataframe['Day'] = dataframe[column_name].dt.dayofweek
-        dataframe['Hour'] = dataframe[column_name].dt.hour
-        dataframe.drop(column_name)
-    else:
+    if dataframe[column_name].dtype != 'datetime64[ns]':
         dataframe[column_name] = pd.to_datetime(dataframe[column_name])
-        dataframe['Day'] = dataframe[column_name].dt.dayofweek
-        dataframe['Hour'] = dataframe[column_name].dt.hour
-        dataframe.drop(column_name)
+   
+    dataframe['Day'] = dataframe[column_name].dt.dayofweek
+    dataframe['Hour'] = dataframe[column_name].dt.hour
+    dataframe.drop(column_name)
+   
     return dataframe
       
      
@@ -81,31 +48,41 @@ def categorical_to_numeric(dataframe: pd.DataFrame, categorical_column: str) -> 
     Returns:
         pd.DataFrame: DataFrame with the one-hot encoding features.
     """
-    encoded_dataframe = pd.get_dummies(dataframe[categorical_column])
+    dataframe = pd.get_dummies(dataframe, columns=[categorical_column])
     dataframe = dataframe.drop(categorical_column)
-    return encoded_dataframe
-
-
-def combine_dataframes(list_of_dataframes: list) -> pd.DataFrame:
-    """
-    The function takes two or more DataFrames of the same size and combines them.
-
-    Args:
-        list_of_dataframes (list): A list of DataFrames.
-
-    Returns:
-        pd.DataFrame: DataFrame with all listed DataFrames combined.
-    """
-    combined_dataframe = pd.concat([list_of_dataframes], axis=1)
-    return combined_dataframe
+    return dataframe
     
     
 def process():
-    df = merge_dataframe()
-    result = fill_null_with_zeros(df)
-    result = create_dataframe(result)
-    result = extract_datetime_features(result)
-    result = categorical_to_numeric()
-    result = combine_dataframes()
-    return result
+    #read dataframes
+    sales_df = pd.read_csv("data\sales_agg.csv")
+    temp_df = pd.read_csv("data\sensor_storage_temperature_agg.csv")
+    stock_df = pd.read_csv("data\sensor_stock_levels_agg.csv")
+    
+    #merge all dataframes
+    merged_df = stock_df.merge(sales_df, on=['timestamp', 'product_id'], how='left')
+    merged_df = merged_df.merge(temp_df, on='timestamp', how='left')
+    
+    #fill missing values
+    merged_df = merged_df.fillna(0)
+    
+    #add additional features from sales table
+    categories_df = create_dataframe(sales_df, ['product_id', 'category'] )
+    categories_df.drop_duplicates()
+    unitPrice_df = create_dataframe(sales_df, ['product_id', 'unit_price'] )
+    unitPrice_df.drop_duplicates()
+    
+    #merge extracted columns with the main dataframe above
+    merged_df = merged_df.merge(categories_df, on='product_id', how='left')
+    merged_df = merged_df.merge(unitPrice_df, on='product_id', how='left')
+    
+    #extract day and hour from the merged dataframe
+    merged_df = extract_datetime_features(merged_df)
+    merged_df = categorical_to_numeric(merged_df, "category")
+    
+    #drop the product id column. no need for machine learning
+    merged_df = merged_df.drop("product_id")
+    merged_df.to_csv()
+    
+    return None
     
